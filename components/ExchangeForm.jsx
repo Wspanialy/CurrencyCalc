f0169023222445efa86336ccd8724a01c65ce3ba
Kacper <witak.kacper@gmail.com>
2023-09-11 13:51:43 +0200

    small stylisation

--- components/ExchangeForm.jsx
+++ components/ExchangeForm.jsx
@@ -26,7 +26,6 @@ const ExchangeForm = () => {
     try {
       const responseCurrencyData = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}/${toCurrency}.json`);
       const data = await responseCurrencyData.json();
-      console.log(data)
       setCurrencyData(data);
     } catch (error) {
       console.error(error);
@@ -35,36 +34,45 @@ const ExchangeForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if(currencyAmount <= 0){
+      console.log("Can not be 0 or less")
+    }
+    else{
     getCurrencyData();
+    }
   }
 
   return (
     <>
     {currencies &&(
-      <form className="" onSubmit={handleSubmit}>
-        <input type="number" value={currencyAmount} onChange={e => setCurrencyAmount(e.target.value)} />
-        <select value={currency} onChange={e => setCurrency(e.target.value)}>
+      <form className="flex flex-col flex-wrap p-3" onSubmit={handleSubmit}>
+        Your money:<input type="number" className="" required value={currencyAmount} onChange={e => setCurrencyAmount(e.target.value)} />
+        <div className="flex-row">
+        Choose your currency: <select value={currency} className="" size="4" onChange={e => setCurrency(e.target.value)}>
           {Object.entries(currencies).map(([currencyCode, currencyName]) => (
             <option key={currencyCode} value={currencyCode}>
               {currencyName}
             </option>
           ))}
         </select>
-        <select value={toCurrency} onChange={e => setToCurrency(e.target.value)}>
+        To which currency you want to exchange?<select value={toCurrency} className="" size="4" onChange={e => setToCurrency(e.target.value)}>
           {Object.entries(currencies).map(([currencyCode, currencyName]) => (
             <option key={currencyCode} value={currencyCode}>
               {currencyName}
             </option>
           ))}
         </select>
-        <button type="submit">Check</button>
+        </div>
+        <button type="submit" className="bg-green-600 text-white font-bold rounded border-2 hover:border-green-800">Check</button>
       </form>
       )}
       {currencyData && (
-        <div>
+        <div className="flex flex-col p-3">
           <p>Your money: {currencyAmount} {(currency).toUpperCase()}</p>
           <p>Your money in the chosen currency: {(currencyAmount * currencyData[toCurrency]).toFixed(2)} {(toCurrency).toUpperCase()}</p>
+          <p>Exchange date information: { currencyData[0] }</p>
         </div>
+        
       )}
       
     </>
